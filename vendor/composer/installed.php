4e4a355e73018addf16cdae5579c71bdfb402956
adytm404 <ogyaadyatma@gmail.com>
2026-07-15 23:32:41 +0700

    build(vendor): bundle composer autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'zenhosta/niagakita',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9c2703ba62adf9bf150fdf0c6f8b6257d8d53897',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'zenhosta/niagakita' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9c2703ba62adf9bf150fdf0c6f8b6257d8d53897',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
